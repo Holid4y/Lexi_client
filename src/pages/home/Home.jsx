1c142e233ab896b1bec0bfe5354653a28321a9a5
Holid4y <68636130+Holid4y@users.noreply.github.com>
2024-05-27 17:51:07 +0300

    update main page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -20,7 +20,7 @@ function Home() {
 
         <main class="container px-4">
 
-            <div class="row">
+            {/* <div class="row">
                 <div class="col-6 col-md-4">
                     <span class="ps-2">Количество слов</span>
                     <div class="card mb-4">
@@ -36,39 +36,53 @@ function Home() {
                 <div class="col-12 col-md-4">
                     <a href="#" class="text-white">
                         <span class="ps-2">Добавить книгу</span>
-                        <div class="card mb-4 w-100 border-none">
+                        <div class="card mb-4 w-100 border-none card-btn">
                             <h4 class="book-text text-center py-2">+</h4>
                         </div>
                     </a>
                 </div>
-            </div>
+            </div> */}
 
-            <div className="row mb-4">
-                <div className="col-8 h-100">
+            <div className="row g-4">
+                <div className="col-12 col-sm-8">
                     <a href="#">
                         <div class="card text-end bg-card-dark">
                             <div class="card-body">
-                                <h4 class="card-title w-75 text-start">Тренировка слов на сегодня</h4>
-                                <p class="text-start mb-0"><span class="book-text">34</span> воспроизведение  <span class="book-text">12</span> написание</p>
+                                <h4 class="card-title text-start">Тренировка слов на сегодня</h4>
+                                <p class="text-start mb-0"><span class="book-text">34</span> воспроизведение  <span class="book-text">12</span> узнаваемость</p>
                             </div>
                         </div>
                     </a>
                 </div>
-                <div class="col-4 d-flex flex-column justify-content-between">
-                    <Link to="/training/reproduce">
-                        <div class="card card-text-lr">
-                            <div class="card-body text-center">
-                                <span>на воспроизведение</span>
+                <div class="col-12 col-sm-4  d-flex flex-column justify-content-between">
+                    <div class="col-12 ps-0">
+                        <Link to="/training/reproduce">
+                            <div class="card card-text-lr card-btn overflow-box">
+                                <div class="card-body text-start">
+                                    <span>Воспроизведение</span>
+                                </div>
+                                <div class="svg-background">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-headphones" viewBox="0 0 16 16">
+                                        <path d="M8 3a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V8a6 6 0 1 1 12 0v5a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h1V8a5 5 0 0 0-5-5"/>
+                                    </svg>
+                                </div>
                             </div>
-                        </div>
-                    </Link>
-                    <Link to="/training/recognize">
-                        <div class="card mt-auto card-text-lr">
-                            <div class="card-body text-center">
-                                <span>на узнаваемость</span>
+                        </Link>
+                    </div>
+                    <dir class="col-12 ps-0">
+                        <Link to="/training/recognize">
+                            <div class="card mt-auto card-text-lr card-btn overflow-box">
+                                <div class="card-body text-start">
+                                    <span>Узнаваемость</span>
+                                </div>
+                                <div class="svg-background">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                                    </svg>
+                                </div>
                             </div>
-                        </div>
-                    </Link>
+                        </Link>
+                    </dir>
                 </div>
                 
             </div>
@@ -89,37 +103,39 @@ function Home() {
                 </div>
             </a>
 
-            <div class="row mb-4">
-                <a class="col-6" href="#">
-                    <div class="card bg-card-dark">
-                        <div class="card-body text-center py-4-5">
-                            <img src="/assets/images/statistic.svg" height={"40px"} alt="" />
-                        </div>
-                    </div>
-                </a>
-                <a class="col-6" href="#">
-                    <div class="card position-relative bg-card-dark">
-                        <div class="card-body text-center py-4-5">
-                            <img src="/assets/images/books.svg" height={"40px"} alt="" />
+            <div class="row mb-4 g-4">
+                <a class="col-6 col-md-3" href="#">
+                    <div class="card position-relative bg-card-dark card-btn">
+                        <div class="card-body text-center py-3">
+                            Все книги
+                            {/* <img src="/assets/images/books.svg" height={"40px"} alt="" /> */}
                         </div>
                         <span class="position-absolute top-0 start-90 translate-middle badge-seccses">15</span>
                     </div>
                 </a>
-            </div>
-            <div class="row">
-                <a class="col-6" href="#">
-                    <div class="card bg-card-dark">
-                        <div class="card-body text-center py-4-5">
-                            <img src="/assets/images/statistic.svg" height={"40px"} alt="" />
+                <a class="col-6 col-md-3" href="#">
+                    <div class="card position-relative bg-card-dark card-btn">
+                        <div class="card-body text-center py-3">
+                            Все слова
+                            {/* <img src="/assets/images/books.svg" height={"40px"} alt="" /> */}
+                        </div>
+                        <span class="position-absolute top-0 start-90 translate-middle badge-seccses">17</span>
+                    </div>
+                </a>
+                <a class="col-6 col-md-3" href="#">
+                    <div class="card bg-card-dark card-btn">
+                        <div class="card-body text-center py-3">
+                            Статистика
+                            {/* <img src="/assets/images/statistic.svg" height={"40px"} alt="" /> */}
                         </div>
                     </div>
                 </a>
-                <a class="col-6" href="#">
-                    <div class="card position-relative bg-card-dark">
-                        <div class="card-body text-center py-4-5">
-                            <img src="/assets/images/books.svg" height={"40px"} alt="" />
+                <a class="col-6 col-md-3" href="#">
+                    <div class="card bg-card-dark card-btn">
+                        <div class="card-body text-center py-3">
+                            История
+                            {/* <img src="/assets/images/statistic.svg" height={"40px"} alt="" /> */}
                         </div>
-                        <span class="position-absolute top-0 start-90 translate-middle badge-seccses">17</span>
                     </div>
                 </a>
             </div>
