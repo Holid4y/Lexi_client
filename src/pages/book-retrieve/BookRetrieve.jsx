3766902621836cdd4057883afc6c2e1b1d92d532
Павел Данилевич <pasha-danilevich@outlook.com>
2024-06-03 14:19:42 +0300

    create BookRetrieveHeader
    вынес BookRetrieveHeader в отдельный компонент

--- src/pages/book-retrieve/BookRetrieve.jsx
+++ src/pages/book-retrieve/BookRetrieve.jsx
@@ -4,11 +4,13 @@ import { useParams } from "react-router-dom";
 
 import { fetchBook } from "../../common/reducers/bookRetrieveSlice";
 import { fetchBookmarksCreateUpdate } from "../../common/reducers/bookmarkSlice";
-import Navigation from "../../common/components/navigation/Navigation"
+
+import Navigation from "../../common/components/navigation/Navigation";
+import BookRetrieveHeader from "./BookRetrieveHeader";
 
 function BookRetrieve() {
   const dispatch = useDispatch();
-  const { book, loading, error } = useSelector(state => state.book);
+  const { book, loading, error } = useSelector((state) => state.book);
   const currentPage = useSelector((state) => state.page);
   const [currentPageIndex, setCurrentPageIndex] = useState(1);
 
@@ -31,16 +33,16 @@ function BookRetrieve() {
   };
 
   const log = {
-    'book': book, 
-    'loading': loading, 
-    'error': error,
-    'currentPage': currentPage 
-  }
-  console.log(log)
+    book: book,
+    loading: loading,
+    error: error,
+    currentPage: currentPage,
+  };
+  console.log(log);
 
   // ---Создание закладки---
 
-  // Когда зарегестрированный пользователь нажимает 
+  // Когда зарегестрированный пользователь нажимает
   // на иконку закладки, надо вызвать:
 
   const createBookmark = () => {
@@ -49,13 +51,15 @@ function BookRetrieve() {
       const targetPage = currentPage;
       dispatch(fetchBookmarksCreateUpdate({ bookId, targetPage }));
     } else {
-      console.error('Ошибка: Невозможно создать закладку. Книга не загружена или отсутствует book.pk');
+      console.error(
+        "Ошибка: Невозможно создать закладку. Книга не загружена или отсутствует book.pk"
+      );
     }
   };
 
   // ---Удаление закладки---
 
-  // Когда зарегестрированный пользователь нажимает 
+  // Когда зарегестрированный пользователь нажимает
   // на иконку закладки, тем самым делая toggle с активной иконки
   // на посивную (ligth), надо вызвать:
 
@@ -63,7 +67,9 @@ function BookRetrieve() {
     if (bookmarkId) {
       dispatch(fetchBookmarksDelete(bookmarkId));
     } else {
-      console.error('Ошибка: Невозможно удалить закладку. Отсутствует bookmarkId');
+      console.error(
+        "Ошибка: Невозможно удалить закладку. Отсутствует bookmarkId"
+      );
     }
   };
 
@@ -74,42 +80,62 @@ function BookRetrieve() {
     const endPage = Math.min(totalPages, currentPageIndex + 1);
 
     const pages = [];
-      for (let i = startPage; i <= endPage; i++) {
-        pages.push(
-          <li key={i} className={`page-item ${i === currentPageIndex ? 'active' : ''}`}>
-            <a className="page-link text-white" href="#" onClick={() => handlePageChange(i)}>
-              {i}
-            </a>
-          </li>
-        );
-      }
-
-      return (
-        <ul className="pagination">
-          <li className={`page-item ${currentPageIndex === 1 ? 'disabled' : ''}`}>
-            <a className="page-link text-white" href="#" aria-label="Previous" onClick={() => handlePageChange(currentPageIndex - 1)}>
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          {pages}
-          <li className={`page-item ${currentPageIndex === totalPages ? 'disabled' : ''}`}>
-            <a className="page-link text-white" href="#" aria-label="Next" onClick={() => handlePageChange(currentPageIndex + 1)}>
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
+    for (let i = startPage; i <= endPage; i++) {
+      pages.push(
+        <li
+          key={i}
+          className={`page-item ${i === currentPageIndex ? "active" : ""}`}
+        >
+          <a
+            className="page-link text-white"
+            href="#"
+            onClick={() => handlePageChange(i)}
+          >
+            {i}
+          </a>
+        </li>
       );
+    }
+
+    return (
+      <ul className="pagination">
+        <li className={`page-item ${currentPageIndex === 1 ? "disabled" : ""}`}>
+          <a
+            className="page-link text-white"
+            href="#"
+            aria-label="Previous"
+            onClick={() => handlePageChange(currentPageIndex - 1)}
+          >
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        {pages}
+        <li
+          className={`page-item ${
+            currentPageIndex === totalPages ? "disabled" : ""
+          }`}
+        >
+          <a
+            className="page-link text-white"
+            href="#"
+            aria-label="Next"
+            onClick={() => handlePageChange(currentPageIndex + 1)}
+          >
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    );
   };
 
   // Одна страница из массива книги
   const getPage = (book) => {
     if (book) {
       const page = book.book[currentPageIndex - 1];
-      return page
+      return page;
     }
-    return []
-  }
-  
+    return [];
+  };
 
   // Создание JSX элементов <p> для каждой строки
   const paragraphs = getPage(book).map((line, index) => (
@@ -117,50 +143,23 @@ function BookRetrieve() {
   ));
 
   return (
-    
     <div>
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
       {book && (
         <div>
-          <div className="container mb-4 pt-2">
-              <a href="/html/book_read.html">
-                  <div className="card text-end mb-4 w-100 bg-card-dark">
-                      <div className="card-body">
-                          <div className="mb-2 card-text-lr">
-                              <h5 className="card-title text-start"><b>{book.title}</b> </h5>
-                              <input type="checkbox" className="btn-check" id="btn-check-4" autocomplete="off" />
-                              <label className="btn" for="btn-check-4">
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
-                                      <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
-                                  </svg>
-                              </label>
-                          </div>
-                          <div className="card-text card-text-lr">
-                              <span><b className="fs-1">{book.page_count}</b> стр</span>
-                              <span>{book.author}</span>
-                          </div>
-                          
-                      </div>
-                  </div>
-              </a>
-          </div>
+          <BookRetrieveHeader/>
           <main className="container px-4">
-                
-              <div className="book-text-read">
-                {paragraphs}
-              </div>
-              
-              <div className="justify-content-center d-flex">
-                {renderPagination()}
-              </div>
+            <div className="book-text-read">{paragraphs}</div>
+            <div className="justify-content-center d-flex">
+              {renderPagination()}
+            </div>
           </main>
           <Navigation />
         </div>
       )}
     </div>
   );
-
 }
 
 export default BookRetrieve;
---
+++ src/pages/book-retrieve/BookRetrieveHeader.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const BookRetrieveHeader = () => {
+  const { book } = useSelector((state) => state.book);
+
+
+  return (
+    <div className="container mb-4 pt-2">
+      <div className="card text-end mb-4 w-100 bg-card-dark">
+        <div className="card-body">
+          <div className="mb-2 card-text-lr">
+            <h5 className="card-title text-start">
+              <b>{book.title}</b>{" "}
+            </h5>
+            <input
+              type="checkbox"
+              className="btn-check"
+              id="btn-check-4"
+              autocomplete="off"
+            />
+            <label className="btn" for="btn-check-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-heart-fill"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+                />
+              </svg>
+            </label>
+          </div>
+          <div className="card-text card-text-lr">
+            <span>
+              <b className="fs-1">{book.page_count}</b> стр
+            </span>
+            <span>{book.author}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookRetrieveHeader;
