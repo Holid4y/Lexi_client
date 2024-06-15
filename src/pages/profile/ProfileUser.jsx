7988ee07eef82e8bac1d6d49defcd0597173beb5
Holid4y <68636130+Holid4y@users.noreply.github.com>
2024-06-16 01:18:32 +0300

    обновил структуру

--- src/pages/profile/ProfileUser.jsx
+++ src/pages/profile/ProfileUser.jsx
@@ -120,6 +120,44 @@ function Profile() {
             />
           </div>
         </div>
+
+        <span className="ps-2">Кол-во ложных вариантов</span>
+        <div class="input-group mb-2">
+            <input
+              type="number"
+              className="form-control py-2"
+              value="4"
+            />
+            <button className="btn btn-outline-success px-3" onClick={() => handleIncrementLevel(index)} type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+              </svg>
+            </button>
+            <button className="btn btn-outline-danger px-3" onClick={() => handleDecrementLevel(index)} type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
+              </svg>
+            </button>
+        </div>
+
+        <span className="ps-2">Кол-во слов в раунде</span>
+        <div class="input-group mb-3">
+            <input
+              type="number"
+              className="form-control py-2"
+              value="10"
+            />
+            <button className="btn btn-outline-success px-3" onClick={() => handleIncrementLevel(index)} type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+              </svg>
+            </button>
+            <button className="btn btn-outline-danger px-3" onClick={() => handleDecrementLevel(index)} type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
+                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
+              </svg>
+            </button>
+        </div>
       </main>
     </div>
   );
