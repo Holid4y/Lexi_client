ed7df794ed64f5976a8ec50ddf0537a502604031
Holid4y <68636130+Holid4y@users.noreply.github.com>
2024-06-16 17:38:27 +0300

    update css + add register front

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setIsAuth } from './common/reducers/authSlice';
 import { setTheme } from './common/reducers/themeSlice';
 import { checkAccessTokenValidity } from './common/reducers/authSlice';
+
 import Navigation from './common/components/Navigation';
+
 import Home from './pages/home/Home';
-import Login from './pages/auth/login/Login';
 import BookList from './pages/books/BookList';
 import BookRetrieve from './pages/book-retrieve/BookRetrieve';
 import BookmarkList from './pages/bookmark-list/BookmarkList';
+
 import Recognize from './pages/training/recognize/Recognize';
 import Reproduce from './pages/training/reproduce/Reproduce';
-import WordList from './pages/word-list-user/WordList';
+
+import Login from './pages/auth/login/Login';
+import Register from './pages/auth/register/Register';
 import Profile from './pages/profile/ProfileUser';
 import LvlSettings from './pages/profile/lvlSettings';
+
+import WordList from './pages/word-list-user/WordList';
 import Statistic from './pages/statistic-user/Statistic';
 
 function App() {
@@ -49,6 +55,7 @@ function App() {
           <Route path="/book/:id" element={<BookRetrieve />} />
           <Route path="/bookmarks" element={<BookmarkList />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/training/recognize" element={<Recognize />} />
           <Route path="/training/reproduce" element={<Reproduce />} />
           <Route path="/word-list" element={<WordList />} />
