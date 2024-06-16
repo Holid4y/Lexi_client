ba234733eb1287e97aa8652525c2b236469fedaa
Holid4y <68636130+Holid4y@users.noreply.github.com>
2024-06-16 16:45:07 +0300

    update css, eth
    Изменил стили
    Добавил верстку страниц с тестами
    Добавил функционал подсказки в тесте
    Исправил Баг с сменой темы, когда обновляешь страницу , выйдя из страницы профиля

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,52 @@
 // React
 import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 // Redux
 import { useDispatch, useSelector } from "react-redux";
 import { setIsAuth } from "./common/reducers/authSlice";
-
+import { setTheme } from './common/reducers/themeSlice';
 // JWT
 import { checkAccessTokenValidity } from './common/reducers/authSlice';
 
+
 import Navigation from './common/components/Navigation';
-
-// Главная страница
 import Home from "./pages/home/Home";
-
-// Авторизация
-import Login from "./pages/auth/login/Login"; // Вход
-
-// Книги
+import Login from "./pages/auth/login/Login";
 import BookList from "./pages/books/BookList";
 import BookRetrieve from "./pages/book-retrieve/BookRetrieve";
 import BookmarkList from "./pages/bookmark-list/BookmarkList";
-
-// Тесты
 import Recognize from './pages/training/recognize/Recognize';
 import Reproduce from './pages/training/reproduce/Reproduce';
 import WordList from './pages/word-list-user/WordList';
-
 import Profile from "./pages/profile/ProfileUser";
 import LvlSettings from "./pages/profile/lvlSettings";
 import Statistic from './pages/statistic-user/Statistic';
 
+
 function App() {
   const dispatch = useDispatch();
   const { isAuth } = useSelector(state => state.auth);
-
-
+  const { darkTheme } = useSelector(state => state.theme);
   
   useEffect(() => {
-    // Вызов функции проверки срока действия access токена при загрузке приложения
-    dispatch(checkAccessTokenValidity())
-  }, []);
+    // Проверяем токен при загрузке приложения
+    dispatch(checkAccessTokenValidity());
+
+    // Загружаем тему из localStorage при загрузке приложения
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      dispatch(setTheme(storedTheme === 'dark'));
+    } else {
+      dispatch(setTheme(window.matchMedia('(prefers-color-scheme: dark)').matches));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    // Применяем тему при изменении значения darkTheme
+    const theme = darkTheme ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [darkTheme]);
 
 
   return (
--- src/pages/profile/ProfileUser.jsx
+++ src/pages/profile/ProfileUser.jsx
@@ -1,39 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSettings } from "../../common/reducers/userSlice";
-
+import { toggleTheme } from "../../common/reducers/themeSlice";  // Импортируем экшн для переключения темы
 import { renderResponse } from "../../../public/urls";
 
 function Profile() {
   const dispatch = useDispatch();
-  const { username, email, activated_email, dark_theme: initialDarkTheme, number_of_false_set, loading, error } = useSelector((state) => state.user);
-
-  const [darkTheme, setDarkTheme] = useState(initialDarkTheme);
+  const { username, email, activated_email, number_of_false_set, loading, error } = useSelector((state) => state.user);
+  const darkTheme = useSelector((state) => state.theme.darkTheme);  // Получаем тему из глобального состояния
 
   useEffect(() => {
     dispatch(fetchSettings());
   }, [dispatch]);
 
-  useEffect(() => {
-    const storedTheme = localStorage.getItem('theme');
-    if (storedTheme) {
-      setDarkTheme(storedTheme === 'dark');
-    } else {
-      setDarkTheme(window.matchMedia('(prefers-color-scheme: dark)').matches);
-    }
-  }, []);
-
-  useEffect(() => {
-    const theme = darkTheme ? 'dark' : 'light';
-    document.documentElement.setAttribute('data-bs-theme', theme);
-    localStorage.setItem('theme', theme);
-  }, [darkTheme]);
-
   const handleThemeChange = () => {
-    setDarkTheme(!darkTheme);
-    // Здесь можно дергать fetchPatchSettings, если нужно сохранить настройку на сервере
+    dispatch(toggleTheme());
   };
 
   return (
@@ -49,17 +31,6 @@ function Profile() {
         </nav>
       </div>
       <main className="container px-4">
-        {/* <div className="mb-3">
-          <label htmlFor="login" className="form-label">
-            Login
-          </label>
-          <input
-            type="text"
-            className="form-control py-2-5"
-            id="login"
-            value={renderResponse(username, '', loading, error)}
-          />
-        </div> */}
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
             Email
@@ -122,41 +93,41 @@ function Profile() {
         </div>
 
         <span className="ps-2">Кол-во ложных вариантов</span>
-        <div class="input-group mb-2">
-            <input
-              type="number"
-              className="form-control py-2"
-              value="4"
-            />
-            <button className="btn btn-outline-success px-3" onClick={() => handleIncrementLevel(index)} type="button">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-              </svg>
-            </button>
-            <button className="btn btn-outline-danger px-3" onClick={() => handleDecrementLevel(index)} type="button">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
-                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
-              </svg>
-            </button>
+        <div className="input-group mb-2">
+          <input
+            type="number"
+            className="form-control py-2"
+            value="4"
+          />
+          <button className="btn btn-outline-success px-3" onClick={() => handleIncrementLevel(index)} type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+            </svg>
+          </button>
+          <button className="btn btn-outline-danger px-3" onClick={() => handleDecrementLevel(index)} type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash" viewBox="0 0 16 16">
+              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
+            </svg>
+          </button>
         </div>
 
         <span className="ps-2">Кол-во слов в раунде</span>
-        <div class="input-group mb-3">
-            <input
-              type="number"
-              className="form-control py-2"
-              value="10"
-            />
-            <button className="btn btn-outline-success px-3" onClick={() => handleIncrementLevel(index)} type="button">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-              </svg>
-            </button>
-            <button className="btn btn-outline-danger px-3" onClick={() => handleDecrementLevel(index)} type="button">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16">
-                <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
-              </svg>
-            </button>
+        <div className="input-group mb-3">
+          <input
+            type="number"
+            className="form-control py-2"
+            value="10"
+          />
+          <button className="btn btn-outline-success px-3" onClick={() => handleIncrementLevel(index)} type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+            </svg>
+          </button>
+          <button className="btn btn-outline-danger px-3" onClick={() => handleDecrementLevel(index)} type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-dash" viewBox="0 0 16 16">
+              <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
+            </svg>
+          </button>
         </div>
       </main>
     </div>
