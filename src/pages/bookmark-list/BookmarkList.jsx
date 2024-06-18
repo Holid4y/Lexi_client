84d2d95765a13e3be8d34be5e7c93be8c06f6b2d
Holid4y <68636130+Holid4y@users.noreply.github.com>
2024-06-18 23:16:48 +0300

    проверка на книги в избранном

--- src/pages/bookmark-list/BookmarkList.jsx
+++ src/pages/bookmark-list/BookmarkList.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
 import { useDispatch, useSelector } from "react-redux";
 import { fetchBookmarks, fetchBookmarksDelete } from "../../common/reducers/bookmarkSlice";
+import { Link } from "react-router-dom";
 
 import Search from "../../common/components/Search";
 import Skeleton from "./components/Skeleton";
@@ -19,18 +18,12 @@ function BookmarkList() {
     dispatch(fetchBookmarks());
   }, [dispatch]);
 
-
   // Обработчик изменения значения поиска
   const handleSearchChange = (value) => {
     setSearchValue(value);
   };
 
-  // ---Удаление закладки---
-
-  // Когда зарегестрированный пользователь нажимает 
-  // на иконку закладки, тем самым делая toggle с активной иконки
-  // на посивную (ligth), надо вызвать:
-
+  // Удаление закладки
   const deleteBookmark = (bookmarkId) => {
     if (bookmarkId) {
       dispatch(fetchBookmarksDelete(bookmarkId));
@@ -38,23 +31,38 @@ function BookmarkList() {
       console.error('Ошибка: Невозможно удалить закладку. Отсутствует bookmarkId');
     }
   };
+
+  const filteredBookmarks = bookmarks?.results?.filter((bookmark) =>
+    bookmark.book_cover.title.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   return (
     <div className="align-items-center">
       <Search onChange={handleSearchChange} />
       <main className="container px-4">
         <div className="row g-4">
-          {(bookmarks && bookmarks.results && bookmarks.results
-              .filter((bookmark) => bookmark.book_cover.title.toLowerCase().includes(searchValue.toLowerCase()) ) // Фильтруем закладки по названию книги
-              .map((bookmark, index) => (              
-                  <BookmarkCard bookmark={bookmark} key={index}/>         
-              ))) ||
-            (loading ? <Skeleton /> : <p>Error: {error}</p>)}
+          {loading ? (
+            <Skeleton />
+          ) : error ? (
+            <p>Error: {error}</p>
+          ) : (
+            <>
+              {filteredBookmarks && filteredBookmarks.length > 0 ? (
+                filteredBookmarks.map((bookmark, index) => (
+                  <BookmarkCard bookmark={bookmark} key={index} />
+                ))
+              ) : (
+                <div className="text-center mt-5">
+                  <h4>У вас пока что нет книг в избранном 😔</h4>
+                </div>
+              )}
+            </>
+          )}
         </div>
         <AddBook />
       </main>
     </div>
-  )
-
+  );
 }
 
 export default BookmarkList;
