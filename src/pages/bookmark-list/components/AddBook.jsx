775c09d2a5621e432398d12055ef0f7c2f220472
Holid4y <68636130+Holid4y@users.noreply.github.com>
2024-06-18 20:19:42 +0300

    update btn "Добавить"

--- src/pages/bookmark-list/components/AddBook.jsx
+++ src/pages/bookmark-list/components/AddBook.jsx
@@ -50,6 +50,8 @@ const AddBook = () => {
 
                         <hr className='my-4' />
 
+                        <input class="form-control form-control-lg" type="text" placeholder="Название книги" />
+
                         <div class="form-check form-switch mt-3">
                             <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
                             <label class="form-check-label" for="flexSwitchCheckDefault">Приватная книга \ текст</label>
