497839404ec6f9ecb735c8d28e9708bce8c22017
Павел Данилевич <pasha-danilevich@outlook.com>
2024-06-22 16:13:31 +0300

    Update Block.jsx

--- src/pages/statistic-user/components/Block.jsx
+++ src/pages/statistic-user/components/Block.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Block({ countWords, index }) {
   return (
-    <div className="col-6 col-md-3">
+    <div className="col-4 col-md-3">
       <div className="card statistic mb-4">
         <h4 className="text-center">{countWords}</h4>
         <span>L{index + 1}</span>
