0fc6dcc7570adc31f9fc3dade35221a8dc916676
Holid4y <68636130+Holid4y@users.noreply.github.com>
2024-06-23 16:51:46 +0300

    && -> ||

--- src/common/components/WordBlockTranslate.jsx
+++ src/common/components/WordBlockTranslate.jsx
@@ -5,7 +5,7 @@ import { toggleWordBlock } from "../reducers/wordSlice";
 const LoadingSpinner = () => (
   <div className="d-flex align-items-center">
     <strong role="status">Loading...</strong>
-    <div class="spinner-border spinner-border-sm ms-auto" role="status" aria-hidden="true"></div>
+    <div className="spinner-border spinner-border-sm ms-auto" role="status" aria-hidden="true"></div>
   </div>
 );
 
@@ -23,12 +23,15 @@ function WordBlockTranslate() {
 
     if (isVisible) {
       document.addEventListener("mousedown", handleClickOutside);
+      document.body.classList.add("no-scroll");
     } else {
       document.removeEventListener("mousedown", handleClickOutside);
+      document.body.classList.remove("no-scroll");
     }
 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
+      document.body.classList.remove("no-scroll");
     };
   }, [isVisible, dispatch]);
 
@@ -37,7 +40,7 @@ function WordBlockTranslate() {
       <div className="modal-backdrop fade show" style={{ display: isVisible ? 'block' : 'none' }}></div>
       <div id="wordBlock" className={`toggle-block ${isVisible ? 'show' : ''}`}>
         <div className="px-3 py-2">
-          {translations && translations.length > 0 && synonyms && synonyms.length > 0 && meanings && meanings.length > 0 ? (
+          {translations && translations.length > 0 || synonyms && synonyms.length > 0 || meanings && meanings.length > 0 ? (
             <div>
               <div style={{ display: 'flex', alignItems: 'center' }}>
                 <span className="fs-2 pe-3 text-capitalize"><b>{text}</b></span>
@@ -46,7 +49,7 @@ function WordBlockTranslate() {
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-heart me-3" viewBox="0 0 16 16">
                       <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-mic-fill" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-mic-fill" viewBox="0 0 16 16">
                       <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0z"/>
                       <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5"/>
                     </svg>
