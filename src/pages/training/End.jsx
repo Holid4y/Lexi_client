7c7e2fa7deaef86738998cb711a35b094376cce6
Павел Данилевич <pasha-danilevich@outlook.com>
2024-06-25 18:02:51 +0300

    fix bug score
    // устанавливаем стайт компонента
            // и после этого очищаем счет
            // так как на след. раундах он нужен чистый

--- src/pages/training/End.jsx
+++ src/pages/training/End.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { useDispatch, useSelector } from "react-redux";
@@ -7,14 +7,25 @@ import { clearScore } from "../../common/reducers/trainingSlice";
 function End({ type, count_word_to_training, setIsEnd }) {
     const dispatch = useDispatch();
     const { score } = useSelector((state) => state.training);
+    const [scoreState, setScoreState] = useState(0)
+
+
+    useEffect(() => {
+        // устанавливаем стэйт компонента
+        // и после этого очищаем счет
+        // так как на след. раундах он нужен чистый
+        setScoreState(score)
+        dispatch(clearScore())
+    }, [dispatch]);
+
     function handleAction() {
         // переключить isEnd на false
         setIsEnd(false)
-        dispatch(clearScore())
+        
     }
     return (
         <>
-            <p>Результат: {score} правильных ответов </p>
+            <p>Результат: {scoreState} правильных ответов </p>
             {(count_word_to_training != 0 & count_word_to_training != null) && (
                 <Link to={`/training/${type}`} onClick={() => handleAction()}>
                     <button>Следующий раунд ({count_word_to_training})</button>
