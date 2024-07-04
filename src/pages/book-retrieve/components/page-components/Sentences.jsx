d8e726af5302cd7e28a7114d9ffd7fde41a4aa68
Павел Данилевич <pasha-danilevich@outlook.com>
2024-07-04 17:09:19 +0300

    хуйня и ваще я в ахуи
    - 4 часа

--- src/pages/book-retrieve/components/page-components/Sentences.jsx
+++ src/pages/book-retrieve/components/page-components/Sentences.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
-import {fetchGoogletrans} from "../../../../common/reducers/googletransSlice"
+import { fetchGoogletrans } from "../../../../common/reducers/googletransSlice";
 
 import SpanWord from "./SpanWord";
 import TranslationButton from "./TranslationButton";
@@ -14,48 +14,84 @@ function Sentences({ sentences, index }) {
     const [translation, setTranslation] = useState(null);
 
     useEffect(() => {
-        setShowTranslation(false)
-        setTranslation(null)
+        setShowTranslation(false);
+        setTranslation(null);
     }, [sentences]);
 
     const handleTranslate = () => {
+        setShowTranslation(true);
+        setTranslation("loading");
         // вызов API перевода
         dispatch(fetchGoogletrans(sentences)) // функция перевода
             .then((response) => {
-                console.log(response);
                 setTranslation(response.payload.translated_text);
-                setShowTranslation(true);
             });
     };
 
-    const [timeTouchStart, setTimeTouchStart] = useState(0)
-    const [timeTouchEnd, setTimeTouchEnd] = useState(0)
+    const [timeTouchStart, setTimeTouchStart] = useState(0);
+    const [timeTouchEnd, setTimeTouchEnd] = useState(0);
+    const [className, setClassName] = useState("sentences");
 
-    const timeLongTouch = 1000
+    const [timePushed, setTimePushed] = useState(0);
 
     useEffect(() => {
-        const timeDifference = timeTouchEnd - timeTouchStart
-        if (timeDifference > timeLongTouch){
-            console.log('это долго')
+
+      
+        if (timeTouchStart !== 0 && timePushed === 0) {
+
+      
+          let interval = setInterval(() => {
+            setTimePushed((prevTime) => prevTime + 100);
+          }, 100);
+      
+          return () => clearInterval(interval);
+        } 
+      
+        if (timeTouchEnd !== 0 && timePushed !== 0) {
+
+          setTimePushed(0);
+        } 
+      }, [timeTouchStart, timeTouchEnd]);
+
+    useEffect(() => {
+        console.log(timePushed)
+        if (timePushed > 300) {
+            setClassName("sentences push-start")
         }
+        if (timePushed > 700) {
+            handleTranslate()
+            setTimeTouchStart(0);
+            setTimeTouchEnd(0)
+        }
+      }, [timePushed]);
+
+
+
+    const handleTouchStart = () => {
+        const now = new Date().getTime()
+        setTimeTouchStart(now);
         
-    }, [timeTouchEnd]);
+    };
+
+    const handleTouchEnd = () => {
+        setTimeTouchEnd(new Date().getTime());
+        setClassName("sentences")
+    };
+
+    const handleContextMenu = (event) => {
+        event.preventDefault();
+        // Здесь вы можете обработать событие контекстного меню
+        // и остановить его распространение
+        event.stopPropagation();
+    };
 
     return (
-        <span 
-        onTouchStart={() => setTimeTouchStart(new Date().getTime())} 
-        key={index} 
-        className="sentences" 
-        onTouchEnd={() => setTimeTouchEnd(new Date().getTime())}
-        >
-            <SpanWord sentences={sentences}/>
+        <span onTouchStart={() => handleTouchStart()} key={index} className={className} onTouchEnd={() => handleTouchEnd()} onContextMenu={handleContextMenu}>
+            <SpanWord sentences={sentences} />
 
             <TranslationButton handleTranslate={handleTranslate} />
-            
-            {showTranslation && (
-                <span className="translation">{translation}</span>
-            )}
 
+            {showTranslation && <span className="translation">{translation}</span>}
         </span>
     );
 }
