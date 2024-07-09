2cb8196783fb57e56506a112465de40f9f2ddb47
Holid4y <68636130+Holid4y@users.noreply.github.com>
2024-07-09 23:46:28 +0300

    update search style

--- src/common/components/Headers/Search.jsx
+++ src/common/components/Headers/Search.jsx
@@ -17,14 +17,14 @@ function Search({ onSearch }) {
     return (
         <div className="container sticky-top mb-3 pt-2">
             <div className="row g-3">
-                <div className="col-10-5">
+                <div className="col-9 col-sm-10 col-md-10-5  col-lg-11">
                     <nav className="navbar dark-nav px-3">
                         <form className="w-100" role="search">
                             <input className="search w-100" type="search" placeholder="Поиск" aria-label="Search" value={searchValue} onChange={handleInputChange} />
                         </form>
                     </nav>
                 </div>
-                <div className="col-1-5">
+                <div className="col-3 col-sm-2 col-md-1-5  col-lg-1">
                     <button className="card-btn bg-card-dark h-100 w-100">
                         <SVG name="filter" />
                     </button>
