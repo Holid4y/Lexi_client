0c31b7389da99e3035373c112c46dd25aab050a5
Павел Данилевич <pasha-danilevich@outlook.com>
2024-08-04 14:29:36 +0300

    Update FormTheme.jsx

--- src/pages/profile/components/form/FormTheme.jsx
+++ src/pages/profile/components/form/FormTheme.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 const FormTheme = ({ themeState, theme, handleThemeChange }) => (
     <div className="form-control mb-3 py-2 d-flex justify-content-between align-items-center">
-        {themeState === theme ? "" : "(не сохранено)"}
+        {themeState === theme ? "" : "*"}
         <select className="form-select" value={themeState} onChange={handleThemeChange}>
             <option value="light">Светлая</option>
             <option value="dark">Темная</option>
             <option value="green">Зеленая</option>
             <option value="red">Красная</option>
         </select>
+        
     </div>
 );
 
