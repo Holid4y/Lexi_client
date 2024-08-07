e842461d31c049883841e011f9b307beb04e7ce4
Holid4y <68636130+Holid4y@users.noreply.github.com>
2024-08-07 16:49:32 +0300

    update home

--- src/pages/home/components/LastReadBook.jsx
+++ src/pages/home/components/LastReadBook.jsx
@@ -22,31 +22,33 @@ function LastReadBook() {
     }, [dispatch]);
 
     return (
-        
-            <div className="card text-end mb-3 w-100 bg-card-dark p-2 animated-block-rightleft">
-                <div className="card-body">
-                    <h3 className="card-title text-start">
-                        {titleLastBook}
-                    </h3>
-                    <div className="row d-flex align-items-end">
-                        <div className="col-6 col-md-8">
-                            <div class="progress bg-progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                                <div class="progress-bar" style={{ width: `${progress}%` }}>
-                                    {progress}%
+            <div className="hover-text-opacity animated-block-leftright">
+                <span className="ps-2 span_hover" id="wordsToLearn">Вы читали ранее</span>
+                <div className="card text-end mb-3 w-100 bg-card-dark p-2 animated-block-rightleft">
+                    <div className="card-body">
+                        <h3 className="card-title text-start">
+                            {titleLastBook}
+                        </h3>
+                        <div className="row d-flex align-items-end">
+                            <div className="col-6 col-md-8">
+                                <div class="progress bg-progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                                    <div class="progress-bar" style={{ width: `${progress}%` }}>
+                                        {progress}%
+                                    </div>
                                 </div>
                             </div>
+                            <div className="col-6 col-md-4">
+                                {continueReadingUrl && (
+                                    <Link to={continueReadingUrl}>
+                                        <button type="button" className="btn btn-primary px-4">
+                                            Продолжить читать
+                                        </button>
+                                    </Link>
+                                )}
+                            </div>
                         </div>
-                        <div className="col-6 col-md-4">
-                            {continueReadingUrl && (
-                                <Link to={continueReadingUrl}>
-                                    <button type="button" className="btn btn-primary px-4">
-                                        Продолжить читать
-                                    </button>
-                                </Link>
-                            )}
-                        </div>
+                        
                     </div>
-                    
                 </div>
             </div>
     );
