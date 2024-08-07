ceca92523d04706317364d050a57559b04afcbeb
Павел Данилевич <pasha-danilevich@outlook.com>
2024-08-07 21:03:03 +0300

    Update BaseNotification.jsx

--- src/common/components/Notification/BaseNotification.jsx
+++ src/common/components/Notification/BaseNotification.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 function BaseNotification({ message, onClose, color, children }) {
     return (
-        <div style={{ backgroundColor: color, padding: '45px', borderRadius: '5px', position: 'relative', width: '300px', height: '200px', display: 'block' }}>
+        <div style={{ backgroundColor: color, padding: '45px', borderRadius: '5px', position: 'relative', width: '300px', display: 'block' }}>
             <span>{message}</span>
+            <hr />
             <button onClick={onClose} style={{ position: 'absolute', top: '5px', right: '5px' }}>Закрыть</button>
             {children}
         </div>
