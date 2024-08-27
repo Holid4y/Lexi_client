cc0bddc9566cda0e018ef706d7d1aba907acf86d
Holid4y <68636130+Holid4y@users.noreply.github.com>
2024-08-27 03:03:01 +0300

    Update InformationUsing.jsx

--- src/pages/landing/InformationUsing.jsx
+++ src/pages/landing/InformationUsing.jsx
@@ -15,7 +15,7 @@ const Instruction = () => {
                     <h3 className="float-md-start mb-0">Lexi</h3>
                     <nav className="nav nav-masthead justify-content-center float-md-end">
                         <Link className="nav-link fw-bold py-1 px-0" aria-current="page" to="/landing">Главная</Link>
-                        <Link className="nav-link fw-bold py-1 px-0 active" to="/instruction">Инструкция</Link>
+                        <Link className="nav-link fw-bold py-1 px-0 active" to="/instruction">Интерактив</Link>
                         <a className="nav-link fw-bold py-1 px-0" href="#">О нас</a>
                     </nav>
                 </header>
