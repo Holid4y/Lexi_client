bdb43005b9bffefc959d97ed21fb59c4174f811a
Holid4y <68636130+Holid4y@users.noreply.github.com>
2024-08-27 03:02:35 +0300

    Update Landing.jsx

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -11,7 +11,7 @@ const Landing = () => {
                     <h3 className="float-md-start mb-0">Lexi</h3>
                     <nav className="nav nav-masthead justify-content-center float-md-end">
                         <a className="nav-link fw-bold py-1 px-0 active" aria-current="page" href="#">Главная</a>
-                        <Link className="nav-link fw-bold py-1 px-0" to="/instruction">Инструкция</Link>
+                        <Link className="nav-link fw-bold py-1 px-0" to="/instruction">Интерактив</Link>
                         <a className="nav-link fw-bold py-1 px-0" href="#">О нас</a>
                     </nav>
                     </div>
