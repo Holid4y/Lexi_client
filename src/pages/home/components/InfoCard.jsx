45778def69015c9b5714902dd25468987d8af3d1
pasha-danilevich <pasha-danilevich@outlook.com>
2024-11-28 13:54:24 +0300

    fix loading info card

--- src/pages/home/components/InfoCard.jsx
+++ src/pages/home/components/InfoCard.jsx
@@ -8,8 +8,14 @@ function InfoCard() {
     const { learning_words, upload_books, loading } = useSelector((state) => state.home);
 
     const getContentOrLoading = (content) => {
-        // Если загрузка идет и контент не загружен, возвращаем компонент Loading и скрываем карту
-        return loading ? <Loading /> : <h4 className="book-text text-center py-2 mb-0 h4_main_block">{content}</h4>;
+        // Проверяем, загружен ли контент
+        if (!content) {
+            // Если контент не загружен, возвращаем компонент Loading
+            return <Loading />;
+        }
+        
+        // Если контент загружен, возвращаем его
+        return content;
     };
 
     return (
@@ -17,60 +23,47 @@ function InfoCard() {
             <div className="col-6 col-md-3 hover-text-opacity animated-block-leftright">
                 <Link to={"/word-list"}>
                     <div className="card card-btn position-relative main-block">
-                        {loading ? (
-                            <Loading />
-                        ) : (
+                        
                             <>
                                 <span className="ps-2 span_main_block text-secondary" id="wordsToLearn">Всего слов</span>
-                                {getContentOrLoading(learning_words)}
+                                <h4 className="book-text text-center py-2 mb-0 h4_main_block">{getContentOrLoading(learning_words)}</h4>
+                                
                             </>
-                        )}
+                        
                     </div>
                 </Link>
             </div>
             <div className="col-6 col-md-3 hover-text-opacity animated-block-leftright">
                 <Link to={"/my-books"}>
                     <div className="card card-btn position-relative main-block">
-                        {loading ? (
-                            <Loading />
-                        ) : (
-                            <>
-                                <span className="ps-2 span_main_block text-secondary" id="uploadBooks">Добавленных книг</span>
-                                {getContentOrLoading(upload_books)}
-                            </>
-                        )}
+                        <>
+                            <span className="ps-2 span_main_block text-secondary" id="uploadBooks">Добавленных книг</span>
+                            <h4 className="book-text text-center py-2 mb-0 h4_main_block">{getContentOrLoading(upload_books)}</h4>
+                        </>
                     </div>
                 </Link>
             </div>
             <div className="col-6 col-md-3 hover-text-opacity animated-block-leftright">
                 <Link to="/statistic">
                     <div className="card card-btn position-relative main-block">
-                    {loading ? (
-                            <Loading />
-                        ) : (
-                            <>
-                                <span className="ps-2 span_main_block text-secondary" id="statistics">Статистика</span>
-                                <h4 className="book-text text-center pb-2 mb-2 h4_main_block">
-                                    <SVG name="statistic" />
-                                </h4>
-                            </>
-                        )}
+                        <>
+                            <span className="ps-2 span_main_block text-secondary" id="statistics">Статистика</span>
+                            <h4 className="book-text text-c  enter pb-2 mb-2 h4_main_block">
+                                <SVG name="statistic" />
+                            </h4>
+                        </>
                     </div>
                 </Link>
             </div>
             <div className="col-6 col-md-3 hover-text-opacity animated-block-leftright">
                 <Link to="/bookmarks">
                     <div className="card card-btn position-relative main-block">
-                    {loading ? (
-                            <Loading />
-                        ) : (
-                            <>
-                                <span className="ps-2 span_main_block text-secondary" id="bookmarks">Закладки</span>
-                                <h4 className="book-text text-center pb-2 mb-2 h4_main_block">
-                                    <SVG name="marklist_fill" />
-                                </h4>
-                            </>
-                        )}
+                        <>
+                            <span className="ps-2 span_main_block text-secondary" id="bookmarks">Закладки</span>
+                            <h4 className="book-text text-center pb-2 mb-2 h4_main_block">
+                                <SVG name="marklist_fill" />
+                            </h4>
+                        </>
                     </div>
                 </Link>
             </div>
